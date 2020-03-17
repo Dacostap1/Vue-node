055e25f48ce65ed98724476f7fa6dbac9b4c36e1
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-17 11:22:03 -0500

    creando un index y config en path

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(morgan('tiny'));
 app.use(cors());
+app.use(express.json());
+//aplication/x-www-form-urlencoded
+app.use(express.urlencoded({ extended:true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.send('holissssss');
