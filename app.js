ad2c327cf29b1983a4f53e4cbefdced55fb5c498
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-17 15:22:32 -0500

    instalando y conf mongoose

--- app.js
+++ app.js
@@ -2,8 +2,17 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
+const mongoose = require('mongoose');
+const uri = 'mongodb://localhost:27017/myapp';
+const options = {useNewUrlParser: true, useCreateIndex: true};
 
 const app = express();
+  
+// Conexion a BD using promises
+ mongoose.connect(uri, options).then(
+    () => { console.log('Conectado a DB') },
+    err => { console.log('err') }
+);
 
 app.use(morgan('tiny'));
 app.use(cors());
@@ -20,7 +29,7 @@ const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('puerto', process.env.PORT || 3000);
+app.set('puerto', process.env.PORT || 4000);
 app.listen(app.get('puerto'), function(){
     console.log('Escuchando puerto: ', app.get('puerto'));
 })
