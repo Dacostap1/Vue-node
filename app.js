34a9dda9f4274d1bc3ff2a170b2fb08178324b6a
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-17 10:04:25 -0500

    instalando nodemon para detectar cambios en servidor

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function(req, res){
-    res.send('holi');
+    res.send('holissssss');
 })
 
 app.listen(3000, function(){
