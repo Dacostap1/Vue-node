109380b8a1e4f53787d9af23da86cded0ef84f59
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-17 11:28:28 -0500

    agregando connect history para vue

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ app.use(cors());
 app.use(express.json());
 //aplication/x-www-form-urlencoded
 app.use(express.urlencoded({ extended:true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.send('holissssss');
 })
+
+// Middleware para Vue.js router modo history
+const history = require('connect-history-api-fallback');
+app.use(history());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), function(){
