140863eb464f2bae3a3bf72c1d6600254e1bed16
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-17 10:54:50 -0500

    instanado y conf Morgan y Cors

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const app = express();
+
+app.use(morgan('tiny'));
+app.use(cors());
 
 app.get('/', function(req, res){
     res.send('holissssss');
 })
 
-app.listen(3000, function(){
-    console.log('Escuchando puerto 3000');
+app.set('puerto', process.env.PORT || 3000);
+app.listen(app.get('puerto'), function(){
+    console.log('Escuchando puerto: ', app.get('puerto'));
 })
