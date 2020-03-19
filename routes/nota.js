410f4a1193226671a034c7dec3b2624600de8149
Daniel Acosta <d4ni3L_15@hotmail.com>
2020-03-19 13:25:04 -0500

    agregando rutas get

--- routes/nota.js
+++ routes/nota.js
@@ -21,5 +21,35 @@ router.post('/nueva-nota', async(req, res)=>{
 
 });
 
+//get -- show one
+router.get('/nota/:id', async(req, res) => {
+
+    const _id = req.params.id;
+
+    try {
+        const notaBD = await Nota.findOne({_id})
+        res.status(200).json(notaBD); //es 200 por defecto
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrio un error',
+            error
+        })
+    }
+});
+
+//get -- show all
+router.get('/notas', async(req, res) => {
+
+    try {
+        const notaBD = await Nota.find();
+        res.json(notaBD); //es 200 por defecto
+    } catch (error) {
+        return res.status(400).json({
+            mensaje: 'Ocurrio un error',
+            error
+        })  
+    }
+});
+
 //Exportando router
 module.exports = router;
